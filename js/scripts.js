743e2c87b8eaaa625242ae4c20c7443eaf1135f9
Azamat Bekmuratov and CJ Rhone <cj@>
2018-03-26 16:10:43 -0700

    Add generate card fuctionality

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("form#deckForm").submit(function(event) {
+    event.preventDefault();
+
+    var numbers = ["ace", "king","queen","joker","10","9","8","7","6","5","4","3","2"]
+    var suits = ["spades", "hearts", "clubs", "diamonds"];
+
+    var output = numbers.forEach(function(number){
+      suits.forEach(function(suit) {
+        $('ul').append('<li>' + number + " of " + suit + '</li>');
+      });
+    });
+
+    $("#results").show();
+
+
+  });
+});
